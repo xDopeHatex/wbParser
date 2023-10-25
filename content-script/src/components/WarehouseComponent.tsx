2920d6e2e9ef0e934a49ed35c4446b7ff08e71c3
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-10-25 11:15:22 +0300

    semantic for button and style

--- content-script/src/components/WarehouseComponent.tsx
+++ content-script/src/components/WarehouseComponent.tsx
@@ -17,13 +17,13 @@ let firstSizeActive: any
 
 function handleMessage(message: any) {
     if (message.type === "COMBINED_PAYLOAD") {
-        const fetchNames = message.data.warehousePayload.names;
+        const fetchNames = message.data.warehousePayload?.names;
         const preFetchRaw = message.data.warehousePayload.payload;
 
 
         const preFinal = preFetchRaw?.sizes?.map((size: any) => size?.stocks?.map((warehouse: any) => {
             const whName = fetchNames.find((whNames: any) => warehouse.wh === whNames.id);
-            return whName ? { ...warehouse, whName: whName.name.substring(0, whName.name.length - 3), name: size.name, origName: size.origName  } : null;
+            return whName ? { ...warehouse, whName: whName?.name?.substring(0, whName?.name?.length - 3), name: size?.name, origName: size?.origName  } : null;
         }))
 
         const final = preFinal.map((arr: any) => arr.sort((a:any , b:any) => b.priority - a.priority))
@@ -38,7 +38,9 @@ function handleMessage(message: any) {
         fetchedRawPayload = fetchRaw
         fetchFinalData = final
 
-        firstSizeActive =  final.find((size: any) => size?.stocks?.length !== 0 )[0].name
+
+        firstSizeActive =  final.find((size: any) => size?.length > 0 )[0].name
+        console.log(firstSizeActive, 'yeeee')
         console.log('raw', fetchedRawPayload)
         console.log('final', fetchFinalData)
 
@@ -94,21 +96,30 @@ function WarehouseComponent() {
         return () => chrome.runtime.onMessage.removeListener(messageListener)
     }, []);
 
-    const activeSizeHandler = (size: any) => {
-       setSizeActive(size)
+    const activeSizeHandler = (qty: number, size: any ) => {
+
+        if (qty > 0) {
+
+            setSizeActive(size)
+        }
+
     }
 
 
-    if (data && rawData && sizeActive) { return  (
+    if (data && rawData && sizeActive) {
+        const fastestWarehouse = data.map((sizes: any) => sizes.filter((item: any) => item?.name == sizeActive )).filter((arr:any) => arr.length !== 0 )[0][0]
+        const allWarehousesArr = data.map((sizes: any) => sizes.filter((item: any) => item?.name == sizeActive )).filter((arr:any) => arr.length !== 0 )[0]
+
+
+        return  (
 
 
         <div className='mediaQueriesLg text-sm rounded-xl bg-white lg:p-[20px] flex flex-col gap-y-4  mb-[24px] mt-[24px] '>
             <div className='text-neutral-700 text-xl'>Раскладка по складам</div>
-            <div className='flex items-center gap-3  flex-wrap'>{rawData?.map((size: any) =>  <div key={size.name} onClick={() => activeSizeHandler(size.name)} className={`rounded-lg border-[1px] py-[3px] px-[12px] flex flex-col  ${size?.stocks?.length === 0 ? ' cursor-not-allowed  bg-neural-400 disabled-button-bg' :    sizeActive == size.name ? "active-button-size  hover:border-teal-500 hover:outline-4 cursor-pointer" :  ' hover:border-teal-500 hover:outline-4 cursor-pointer'  }`} ><span className={`${size?.stocks?.length === 0 ? "disabled-text-color" :  '' }`}>{size.origName}</span><span className={`text-xs ${size.stocks.length !== 0 ? "text-neutral-400" : 'disabled-text-color'}`}>{size.name}</span></div> )}</div>
-            {data.map((sizes: any) => sizes.filter((item: any) => item.name == sizeActive )).filter((arr:any) => arr.length !== 0 )[0].map((item:any, index: number) => index === 1 ?  (<div className='text-neutral-700 text-lg'>{item.whName}: <span>{item.time1 + item.time2} час.</span> <span className='inline-block h-5 w-5'><BoltIcon/></span></div> ) : null)}
-            <div className='border-t-[1px] border-teal-500'></div>
-            {data.map((sizes: any) => sizes.filter((item: any) => item.name == sizeActive )).filter((arr:any) => arr.length !== 0 )[0].map((item:any, index: number) => index >= 1 ?  (<div key={item.wh} className='flex items-center'><span className='flex-1'>{item.whName}</span><span>{item.time1 + item.time2} ч.</span><span>{item.qty} шт.</span></div>) : null)}
-
+            <div className='flex items-center gap-3  flex-wrap'>{rawData.map((size: any) =>  <button key={size.name} onClick={() => activeSizeHandler(size.stocks.length, size.name)}  className={`rounded-lg button-border-style py-[3px] px-[12px] flex flex-col  ${size.stocks?.length === 0 ? ' cursor-not-allowed  bg-neural-400 disabled-button-bg' :    sizeActive == size.name ? "active-button-size  hover:border-teal-500 hover:outline-4 cursor-pointer" :  ' hover:border-teal-500 hover:outline-4 cursor-pointer'  }`} ><span className={`${size?.stocks.length === 0 ? "disabled-text-color" :  '' }`}>{size?.origName}</span><span className={`text-xs ${size?.stocks?.length !== 0 ? "text-neutral-400" : 'disabled-text-color'}`}>{size?.name}</span></button> )}</div>
+            {<div className='text-neutral-700 text-lg'>{fastestWarehouse?.whName}: <span>{fastestWarehouse?.time1 + fastestWarehouse?.time2} час.</span> <span className='inline-block h-5 w-5 text-teal-200'><BoltIcon/></span></div> }
+            <div className='border-t-[1px] border-teal-200'></div>
+            {allWarehousesArr?.map((item:any) =>  <div key={item?.wh} className='flex items-center'><span className='flex-1'>{item?.whName}</span><span>{item?.time1 + item?.time2} ч.</span><span>{item?.qty} шт.</span></div>)}
         </div>
 
 
